310b1f5eb3dd02538ecaf58fdbaf52e2cfa5ad31
Keith McCartney <keithtmccartney@hotmail.com>
2022-01-09 10:25:38 +0000

    Adding a simple test with Pass-results.

---
+++ miniature-engine/StringCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniature_engine
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            int add = 0;
+
+            try
+            {
+                add = Convert.ToInt32(numbers);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return add;
+        }
+    }
+}
